9793ff70315e32b9c662a99f93461a4f444c62ca
Justin Seubert <dude051@gmail.com>
2017-02-17 16:22:31 -0600

    Fix node install to copy libraries

--- codedeploy/install_node.sh
+++ codedeploy/install_node.sh
@@ -4,10 +4,12 @@
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${SCRIPTS_DIR}/variables.sh
 
-NODE_VERSION=node-v6.9.5-linux-x64
-NODE_FILE=$NODE_VERSION.tar.xz
+NODE_VERSION="v6.9.5"
 
 cd /tmp
-curl -O https://nodejs.org/dist/v6.9.5/$NODE_FILE
-tar -xvf $NODE_FILE
-cp $NODE_VERSION/bin/* /usr/local/bin/
+curl -LO http://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-linux-x64.tar.gz
+tar xzf node-$NODE_VERSION-linux-x64.tar.gz
+cp -rp node-$NODE_VERSION-linux-x64 /usr/local/
+ln -s /usr/local/node-$NODE_VERSION-linux-x64 /usr/local/node
+ln -s /usr/local/node-$NODE_VERSION-linux-x64/bin/node /usr/local/bin/node
+ln -s /usr/local/node-$NODE_VERSION-linux-x64/bin/npm /usr/local/bin/npm
