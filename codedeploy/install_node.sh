64534818922e6afd50c7eacab92ca6c81f086784
Justin Seubert <dude051@gmail.com>
2017-02-17 16:04:32 -0600

    Fix node install

--- codedeploy/install_node.sh
+++ codedeploy/install_node.sh
@@ -4,9 +4,10 @@
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${SCRIPTS_DIR}/variables.sh
 
-NODE_FILE=node-v6.9.5-linux-x64.tar.xz
+NODE_VERSION=node-v6.9.5-linux-x64.tar.xz
+NODE_FILE=$NODE_VERSION.tar.xz
 
 cd /tmp
 curl -O https://nodejs.org/dist/v6.9.5/$NODE_FILE
 tar -xvf $NODE_FILE
-cp $NODE_file/bin/* /usr/local/bin/
+cp $NODE_VERSION/bin/* /usr/local/bin/
